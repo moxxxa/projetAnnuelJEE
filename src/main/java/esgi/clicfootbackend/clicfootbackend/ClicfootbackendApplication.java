a4cb56b385b401e783246b43823d8bbd99991997
Moxa <mohaxoy98@gmail.com>
2020-06-07 20:29:49 +0200

    Global cors configuration

--- src/main/java/esgi/clicfootbackend/clicfootbackend/ClicfootbackendApplication.java
+++ src/main/java/esgi/clicfootbackend/clicfootbackend/ClicfootbackendApplication.java
@@ -2,6 +2,13 @@ package esgi.clicfootbackend.clicfootbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class ClicfootbackendApplication {
@@ -10,4 +17,16 @@ public class ClicfootbackendApplication {
         SpringApplication.run(ClicfootbackendApplication.class, args);
     }
 
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+        //  replace ** with the url of the front-side one deployed on azure
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
