c4058b0ff9eb217391a7326d12a9c9c734482a49
Moxa <mohaxoy98@gmail.com>
2020-06-16 20:28:16 +0200

    adding to cors to the security config file

--- src/main/java/esgi/clicfootbackend/clicfootbackend/configuration/SecurityConfig.java
+++ src/main/java/esgi/clicfootbackend/clicfootbackend/configuration/SecurityConfig.java
@@ -3,6 +3,7 @@ package esgi.clicfootbackend.clicfootbackend.configuration;
 import esgi.clicfootbackend.clicfootbackend.authentication.AppAuthProvider;
 import esgi.clicfootbackend.clicfootbackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +15,7 @@ import org.springframework.security.web.authentication.Http403ForbiddenEntryPoin
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
+import org.springframework.security.web.session.SessionManagementFilter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -35,12 +37,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.authenticationProvider(authProvider);
     }
 
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
+                .csrf().disable()
                 .exceptionHandling()
                 .authenticationEntryPoint(new Http403ForbiddenEntryPoint(){})
                 .and()
+                .cors().and()
                 .authenticationProvider(authProvider)
                 .formLogin()
                 .loginProcessingUrl("/login")
